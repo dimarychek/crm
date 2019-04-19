5b4e54bce6717c41dc405b59fb2fb555ac0faac6
Dmitriy Rychek <dimarychek@gmail.com>
2019-04-19 11:28:05 +0300

    admin lte fixes

--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -3,33 +3,35 @@
 @section('content')
     <div class="row">
         <div class="col-sm-8 offset-sm-2">
-            <h1 class="display-3">Add</h1>
-            <div>
-                @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div><br />
-                @endif
-                <form method="post" action="{{ route('companies.store') }}">
-                    @csrf
-                    <div class="form-group">
-                        <label for="name">Name:</label>
-                        <input type="text" class="form-control" name="name" value="{{ old('name') }}"/>
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email:</label>
-                        <input type="text" class="form-control" name="email" value="{{ old('email') }}"/>
-                    </div>
-                    <div class="form-group">
-                        <label for="website">Website:</label>
-                        <input type="text" class="form-control" name="website" value="{{ old('website') }}"/>
-                    </div>
-                    <button type="submit" class="btn btn-primary-outline">Save</button>
-                </form>
+            <div class="box box-primary">
+                <h1 class="display-3 box-header">Add</h1>
+                <div class="box-body">
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div><br />
+                    @endif
+                    <form method="post" action="{{ route('companies.store') }}">
+                        @csrf
+                        <div class="form-group">
+                            <label for="name">Name:</label>
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email:</label>
+                            <input type="text" class="form-control" name="email" value="{{ old('email') }}"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="website">Website:</label>
+                            <input type="text" class="form-control" name="website" value="{{ old('website') }}"/>
+                        </div>
+                        <button type="submit" class="btn btn-primary-outline">Save</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
